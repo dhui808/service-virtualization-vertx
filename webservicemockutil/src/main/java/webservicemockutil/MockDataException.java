11fa8742c24d21d1ae7084db0fe448c190e8aa7b
dannyhui <huichunwuca@yahoo.com>
2020-07-17 13:42:51 -0400

    remove old utils

--- webservicemockutil/src/main/java/webservicemockutil/MockDataException.java
+++
@@ -1,9 +0,0 @@
-package webservicemockutil;
-
-public class MockDataException extends RuntimeException {
-	private static final long serialVersionUID = 8056197247026661804L;
-	
-	public MockDataException(String msg) {
-		super(msg);
-	}
-}
